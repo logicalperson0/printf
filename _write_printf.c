25911222e7c1d3a83dd7c2471c3046fbfee12b43
Samuel <a2jsamuel@gmail.com>
2023-01-23 15:20:14 +0000

    fix

--- _write_printf.c
+++ _write_printf.c
@@ -13,12 +13,14 @@ int _printfs(char *c)
 
 	if (c == NULL)
 	{
-		return (-1);
+		write(1, "(null)", 6)
+		return (6);
 	}
 	else
 	{
 		while (c[i] != '\0')
 			i++;
-		return (write(1, c, i));
+		printfchar(c[i]);
 	}
+	return (i);
 }
