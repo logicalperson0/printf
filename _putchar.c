722f944308ce4aebbf4eee60a0eae5bdd1ca30ac
paulgi48 <paulgiovannio@outlook.com>
2023-01-24 20:47:16 +0100

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -4,7 +4,6 @@
 /**
  * _printfchar - writes the character c to stdout
  * @c: The character to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
